a21775af4230b06975a2cd1af747165db877bf0d
Daniel Sloof <goapsychadelic@gmail.com>
2002-01-01 01:22:12 +0100

    add a bug

---
+++ test.php
@@ -0,0 +1,29 @@
+<?php var_dump($_POST['a'][52]); ?>
+
+<form action="test.php" method="post" enctype="multipart/form-data">
+
+<input type="hidden" name="validate_datetime_35" />
+<input type="hidden" name="validate_datetime_36" />
+<input type="hidden" name="validate_datetime_37" />
+<input type="hidden" name="validate_datetime_38" />
+<input type="hidden" name="validate_datetime_39" />
+<input type="hidden" name="validate_datetime_40" />
+<input type="hidden" name="validate_datetime_57" />
+<input type="hidden" name="validate_datetime_41" />
+<input type="hidden" name="validate_datetime_42" />
+<input type="hidden" name="validate_datetime_43" />
+<input type="hidden" name="validate_datetime_44" />
+<input type="hidden" name="validate_datetime_45" />
+<input type="hidden" name="validate_datetime_46" />
+<input type="hidden" name="validate_datetime_56" />
+<input type="hidden" name="validate_datetime_51" />
+<input type="hidden" name="validate_datetime_50" />
+<input type="hidden" name="validate_datetime_49" />
+<input type="hidden" name="validate_datetime_48" />
+<input type="hidden" name="validate_datetime_47" />
+
+<input type="checkbox" name="a[52][]" value="22" checked="checked" />
+
+<input type="submit" />
+
+</form>
